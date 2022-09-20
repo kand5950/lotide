24fa2f2a1ee1d739e6da5f13616de23f0f4fa17e
Anit <anittankandasamy@gmail.com>
2022-09-20 05:26:02 +0000

    created a new function to get head of array and assert it

---
+++ head.js
@@ -0,0 +1,24 @@
+
+const head = function(actual) {
+
+  return actual[0];
+
+};
+
+const assertEqual = function(actual, expected) {
+  
+  if (actual === expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} === ${expected}`);
+  
+  } else {
+    console.log(`✅✅✅ Assertion Passed: ${actual} !== ${expected}`);
+  }
+  
+};
+
+
+
+
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
