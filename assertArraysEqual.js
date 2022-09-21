d1bb099ab4c8f8061f8a705b68800df1c5f961c0
Anit <anittankandasamy@gmail.com>
2022-09-21 05:29:18 +0000

    created function to find if arrays are equal

---
+++ assertArraysEqual.js
@@ -0,0 +1,37 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+
+const eqArrays = function(actual, expected) {
+  if (actual.length !== expected.length) {
+  return false;
+    } else {
+      for (let i=0; i < actual.length; i++){
+        if(actual[i] !== expected[i]){
+          return false;
+        }
+      }
+      return true;
+  }
+};
+
+const assertArraysEqual = function (actual, expected){
+
+  if (eqArrays(actual, expected)){
+      console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    
+    } else {
+      console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    }
+  };
+
+
+assertArraysEqual(([1, 2, 3], [1, 2, 3]), ([1, 2, 3], [1, 2, 3])); // => should PASS
