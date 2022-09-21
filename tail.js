bd4313125d473d9279e891d05d68087b13e60562
Anit <anittankandasamy@gmail.com>
2022-09-21 04:34:56 +0000

    fixed assertion logs

--- tail.js
+++ tail.js
@@ -9,10 +9,10 @@ const result = tail(["Hello", "Lighthouse", "Labs"]);
 const assertEqual = function(actual, expected) {
 
   if (actual === expected) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} === ${expected}`);
+    console.log(`✅✅✅ Assertion Passed: ${actual} !== ${expected}`);
   
   } else {
-    console.log(`✅✅✅ Assertion Passed: ${actual} !== ${expected}`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} === ${expected}`);
   }
 
 };
