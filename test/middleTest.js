93a502907389b5a5caea020260aad8f08588382a
Anit <anittankandasamy@gmail.com>
2022-09-27 01:58:10 +0000

    added chai and mocha

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,27 @@
-const eqArrays = require('../eqArrays');
+/* const eqArrays = require('../eqArrays');
 
 const assertArraysEqual = require('../assertArraysEqual');
 
 const middle = require('../middle');
 
-console.log(middle([1, 2, 3, 4, 5])) // => [3]
+console.log(middle([1, 2, 3, 4, 5])) // => [3] */
+
+const assert = require('chai').assert;
+const middle   = require('../middle');
+
+describe("#middle", () => {
+  it('returns middle element for ([1, 2, 3])', () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  })
+
+  it('returns middle element for ([1, 2, 3, 4, 5])', () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  })
+
+  it('returns [] element for ([1])', () => {
+    assert.deepEqual(middle([1]), []);
+  })
+
+
+
+});
