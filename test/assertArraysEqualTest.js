0d016a3b631bf06e5c17ddbce21a160ff513943c
Anit <anittankandasamy@gmail.com>
2022-09-27 00:34:00 +0000

    updated using export and require

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,3 +1,5 @@
 const assertArraysEqual = require('../assertArraysEqual');
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays');
 
 assertArraysEqual(([1, 2, 3], [1, 2, 3]), ([1, 2, 3], [1, 2, 3])); // => should PASS
